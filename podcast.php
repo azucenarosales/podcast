7ce9b9ba3bd14dd237966256a6a8e75c6fab3b94
azucenarosales <azucena.rosales@gmail.com>
2016-12-26 23:26:47 -0600

    Update podcast.php
    CURL

--- podcast.php
+++ podcast.php
@@ -5,23 +5,29 @@
     <?php
 
     $rssLink = $_POST['rss'];
-    $contenido = implode(file($rssLink));
-    $xml = new SimpleXmlElement($contenido);
+    $curl_handle=curl_init();
+  curl_setopt($curl_handle, CURLOPT_URL, $rssLink);
+  curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+  curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Podcast');
+  $contenido = curl_exec($curl_handle);
+  curl_close($curl_handle);  
+  $xml = new SimpleXmlElement($contenido);
+
 
     ?>
     <ul>
       <!-- Toma el xml parseado e imprime su titulo, la fecha de publicación y la descripción  -->
       <?php foreach ($xml->channel->item as $item):?>
+
       <h5 class="title"><a href="<?=$item->enclosure->attributes()->url?>" class="link"><?=$item->title?></a> <a href="<?=$item->enclosure->attributes()->url?>" class="link"><i class="material-icons">play_arrow</i></a> </h5>
       <p><?=$item->pubDate?></p>
-      <?=$item->description?>
+      <?php // Cambia los h1 al h6 por un parrafo en negrita para que no pierda el estilo
+      echo preg_replace('/<h[1-6](.*?)<\/h[1-6]>/si', '<p style="font-weight:bold"$1</p>', $item->description);
+      ?>
       <?php endforeach;?>
     </ul>
     </div>
   </div>	
 </body>
 </html>
-
-
-
-
